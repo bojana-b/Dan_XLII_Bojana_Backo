cfe4e52b668731bbf66ee29fc43835cef80c0a91
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-07-01 09:12:00 +0200

    Add Service.cs, Employee.cs, Command

---
+++ Dan_XLII_Bojana_Backo/Dan_XLII_Bojana_Backo/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dan_XLII_Bojana_Backo
+{
+    class Employee
+    {
+        public string NameSurname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int CardID { get; set; }
+        public string JMBG { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public string Sector { get; set; }
+        public string Location { get; set; }
+        public string Manager { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string nameSurname, DateTime dateOfBirth, int cardID, string jMBG, string gender, string phone, string sector, string location, string manager)
+        {
+            NameSurname = nameSurname;
+            DateOfBirth = dateOfBirth;
+            CardID = cardID;
+            JMBG = jMBG;
+            Gender = gender;
+            Phone = phone;
+            Sector = sector;
+            Location = location;
+            Manager = manager;
+        }
+    }
+}
---
+++ Dan_XLII_Bojana_Backo/Dan_XLII_Bojana_Backo/Service.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dan_XLII_Bojana_Backo
+{
+    public class Service
+    {
+        // Method that reads all employees from database
+        public List<tblEmployee> GetAllEmployees()
+        {
+            try
+            {
+                using(EmployeeEntities context = new EmployeeEntities())
+                {
+                    List<tblEmployee> list = new List<tblEmployee>();
+                    list = (from x in context.tblEmployees select x).ToList();
+                    return list;
+                }
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+
+        // Method that adds employee to database
+        public tblEmployee AddEmployee(tblEmployee employee)
+        {
+            try
+            {
+                using(EmployeeEntities context = new EmployeeEntities())
+                {
+                    tblEmployee newEmployee = new tblEmployee();
+                    newEmployee.NameSurname = employee.NameSurname;
+                    newEmployee.DateOfBirth = employee.DateOfBirth;
+                    newEmployee.CardID = employee.CardID;
+                    newEmployee.JMBG = employee.JMBG;
+                    newEmployee.Gender = employee.Gender;
+                    newEmployee.Phone = employee.Phone;
+                    newEmployee.Sector = employee.Sector;
+                    newEmployee.Location = employee.Location;
+                    newEmployee.Manager = employee.Manager;
+
+                    context.tblEmployees.Add(newEmployee);
+                    context.SaveChanges();
+
+                    return employee;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+
+        // Method that deletes employee from database based on forwared idCard
+        public void DeleteEmployee(int idCard)
+        {
+            try
+            {
+                using(EmployeeEntities context = new EmployeeEntities())
+                {
+                    tblEmployee employeeToDelete = (from p in context.tblEmployees where p.CardID == idCard select p).First();
+                    context.tblEmployees.Remove(employeeToDelete);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception" + ex.Message.ToString());
+            }
+        }
+    }
+}
